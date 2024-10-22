f3c4c3477b082ae6a280cc42eac3bb3ac1c54e92
Eavilesz <ernesto-av@hotmail.com>
2024-10-22 11:24:17 -0500

    Added more cards

---
+++ src/components/portfolioSection.jsx
@@ -0,0 +1,129 @@
+import { useState } from 'react';
+import Image from 'next/image';
+import { motion, AnimatePresence } from 'framer-motion';
+import { ExternalLink, ChevronDown, ChevronUp } from 'lucide-react';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+import { useTranslations } from 'next-intl';
+
+export default function PortfolioSection({ portfolioObj }) {
+  const [hoveredProject, setHoveredProject] = useState(null);
+  const [expandedDescriptions, setExpandedDescriptions] = useState({});
+
+  const t = useTranslations('Portfolio');
+
+  const toggleDescription = (index) => {
+    setExpandedDescriptions((prev) => ({
+      ...prev,
+      [index]: !prev[index],
+    }));
+  };
+
+  return (
+    <section>
+      <h2 className="text-4xl font-bold mb-12 text-center text-teal-300">
+        {portfolioObj.title}
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {portfolioObj.projects.map((project, index) => (
+          <motion.div
+            key={project.title}
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: index * 0.1 }}
+          >
+            <Card
+              className="bg-black/20 border-teal-600 overflow-hidden h-full flex flex-col"
+              onMouseEnter={() => setHoveredProject(index)}
+              onMouseLeave={() => setHoveredProject(null)}
+            >
+              <motion.div
+                animate={{
+                  scale: hoveredProject === index ? 1.05 : 1,
+                }}
+                transition={{ duration: 0.3 }}
+              >
+                <Image
+                  src={project.image}
+                  alt={project.title}
+                  width={300}
+                  height={200}
+                  className="w-full object-cover h-64"
+                />
+                <CardHeader className="mb-4">
+                  <CardTitle className="text-teal-300 mb-2 text-xl">
+                    {project.title}
+                  </CardTitle>
+                  <Button
+                    variant="ghost"
+                    className="text-teal-400 hover:text-teal-300 p-0 h-auto"
+                    onClick={() => toggleDescription(index)}
+                  >
+                    {expandedDescriptions[index] ? (
+                      <>
+                        {t('hideDescription')}
+                        <ChevronUp className="ml-2 h-4 w-4" />
+                      </>
+                    ) : (
+                      <>
+                        {t('seeDescription')}
+                        <ChevronDown className="ml-2 h-4 w-4" />
+                      </>
+                    )}
+                  </Button>
+                </CardHeader>
+                <CardContent className="flex-grow">
+                  <AnimatePresence>
+                    {expandedDescriptions[index] && (
+                      <motion.div
+                        initial={{ opacity: 0, height: 0 }}
+                        animate={{ opacity: 1, height: 'auto' }}
+                        exit={{ opacity: 0, height: 0 }}
+                        transition={{ duration: 0.3 }}
+                      >
+                        <CardDescription className="text-white text-md mb-4">
+                          {project.description}
+                        </CardDescription>
+                      </motion.div>
+                    )}
+                  </AnimatePresence>
+                  <div className="flex flex-wrap gap-3 min-h-20">
+                    {project.tags.map((tag) => (
+                      <Badge
+                        key={tag.name}
+                        variant="secondary"
+                        className="flex items-center gap-1 h-min bg-teal-100/20 text-white"
+                      >
+                        <tag.icon color={tag.iconColor} size={'22px'} />
+                        {tag.name}
+                      </Badge>
+                    ))}
+                  </div>
+                </CardContent>
+                <CardFooter>
+                  <a
+                    href={project.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-teal-400 hover:text-teal-300 flex items-center gap-1"
+                  >
+                    {t('viewProject')}
+                    <ExternalLink size={16} />
+                  </a>
+                </CardFooter>
+              </motion.div>
+            </Card>
+          </motion.div>
+        ))}
+      </div>
+    </section>
+  );
+}
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,139 @@
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge"
+import { clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+import { RiNextjsLine } from 'react-icons/ri';
+import { RiJavascriptLine } from 'react-icons/ri';
+import { TbBrandMongodb } from 'react-icons/tb';
+import { RiTailwindCssFill } from 'react-icons/ri';
+import { SiTestinglibrary } from 'react-icons/si';
+import { BiLogoTypescript } from 'react-icons/bi';
+import { FaGear } from 'react-icons/fa6';
+import { TbBrandDjango } from 'react-icons/tb';
+import { FaCss3Alt } from 'react-icons/fa';
+import { FaGitAlt } from 'react-icons/fa6';
+import { DiScrum } from 'react-icons/di';
+import { FaReact } from 'react-icons/fa6';
+import { FaFigma } from 'react-icons/fa';
+import { FaSass } from 'react-icons/fa';
+import { SiJest } from 'react-icons/si';
+
+import spaceNews from '@/assets/space-news.png';
+import sttoria from '@/assets/sttoria.avif';
+import taskManager from '@/assets/task-manager.avif';
+import twilio from '@/assets/twilio.avif';
+import jelou from '@/assets/jelou.avif';
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+export const getPortfolioProjects = (t) => ({
+  title: t('projectsTitle'),
+  projects: [
+    {
+      title: t('newsTitle'),
+      description: t.rich('newsDescription'),
+      image: spaceNews,
+      tags: [
+        { name: 'Nextjs', icon: RiNextjsLine, iconColor: '#000000' },
+        { name: 'JavaScript', icon: RiJavascriptLine, iconColor: '#F0DB4F' },
+        { name: 'MongoDB', icon: TbBrandMongodb, iconColor: '#3FA037' },
+        {
+          name: 'Tailwind CSS',
+          icon: RiTailwindCssFill,
+          iconColor: '#38bdf9',
+        },
+        { name: 'API Integration', icon: FaGear, iconColor: '#38bdf9' },
+      ],
+      url: 'https://news-two-neon.vercel.app/',
+    },
+    {
+      title: t('tasksTitle'),
+      description: t('taskManagerDescription'),
+      image: taskManager,
+      tags: [
+        { name: 'Nextjs', icon: RiNextjsLine, iconColor: '#000000' },
+        { name: 'TypeScript', icon: BiLogoTypescript, iconColor: '#3178c6' },
+        {
+          name: 'Tailwind CSS',
+          icon: RiTailwindCssFill,
+          iconColor: '#38bdf9',
+        },
+        { name: 'API Integration', icon: FaGear, iconColor: '#38bdf9' },
+        {
+          name: 'Testing Library',
+          icon: SiTestinglibrary,
+          iconColor: '#d52e2c',
+        },
+      ],
+      url: 'https://tasks-chi-two.vercel.app/',
+    },
+    {
+      title: t('sttoriaTitle'),
+      description: t('SttoriaDescription'),
+      image: sttoria,
+      tags: [
+        { name: 'Nextjs', icon: RiNextjsLine, iconColor: '#000000' },
+        { name: 'JavaScript', icon: RiJavascriptLine, iconColor: '#F0DB4F' },
+        {
+          name: 'Tailwind CSS',
+          icon: RiTailwindCssFill,
+          iconColor: '#38bdf9',
+        },
+      ],
+      url: 'https://sttoria.vercel.app/',
+    },
+  ],
+});
+
+export const getPortfolioExperience = (t) => ({
+  title: t('experienceTitle'),
+  projects: [
+    {
+      title: t('twilioTitle'),
+      description: t.rich('twilioDescription'),
+      image: twilio,
+      tags: [
+        { name: 'Nextjs', icon: RiNextjsLine, iconColor: '#000000' },
+        { name: 'Django', icon: TbBrandDjango, iconColor: '#092e20' },
+        { name: 'TypeScript', icon: BiLogoTypescript, iconColor: '#3178c6' },
+        {
+          name: 'CSS',
+          icon: FaCss3Alt,
+          iconColor: '#2965f1',
+        },
+        { name: 'API Integration', icon: FaGear, iconColor: '#38bdf9' },
+        { name: 'GIT', icon: FaGitAlt, iconColor: '#F1502F' },
+        { name: 'Scrum', icon: DiScrum, iconColor: '#40E0D0' },
+        { name: 'Jest', icon: SiJest, iconColor: '#EEC5B9' },
+      ],
+      url: 'https://www.twilio.com/docs',
+    },
+    {
+      title: t('jelouTitle'),
+      description: t('jelouDescription'),
+      image: jelou,
+      tags: [
+        { name: 'React', icon: FaReact, iconColor: '#61dbfb' },
+        { name: 'JavaScript', icon: RiJavascriptLine, iconColor: '#F0DB4F' },
+        {
+          name: 'Tailwind CSS',
+          icon: RiTailwindCssFill,
+          iconColor: '#38bdf9',
+        },
+        { name: 'API Integration', icon: FaGear, iconColor: '#38bdf9' },
+        {
+          name: 'Figma',
+          icon: FaFigma,
+          iconColor: '#a259ff',
+        },
+        {
+          name: 'Sass',
+          icon: FaSass,
+          iconColor: '#CD6799',
+        },
+      ],
+      url: 'https://jelou.ai/',
+    },
+  ],
+});
